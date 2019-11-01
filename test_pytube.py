b5822cac5ab3c2541b23c6056403fdd39ef62566
Rosin <asakhno@gmail.com>
2019-11-01 16:12:47 +0300

    Добавил версию pytube для этого файла

---
+++ test_pytube.py
@@ -0,0 +1,25 @@
+# Скрипт для скачивания роликов с Ютуба
+# 01.11.2019
+# использует pytube Version: 9.5.2
+# с коррекцией  return self.player_config_args['title'] в pytube\__main__.py
+
+from pytube import YouTube
+# URL для отладки
+# https://www.youtube.com/watch?v=pGQGVJdDO7U
+
+# пп вызывается в процессе загрузки и печатает процент загруженного
+def progress_function(stream, chunk, file_handle, bytes_remaining):
+	p  = round((1-bytes_remaining/video.filesize)*100, 3)
+	ostatok = p % 5
+	if ostatok == 0:
+		print( p, '% получено')
+
+video_link = input('Задайте URL скачиваемого видео ')
+yt = YouTube(url=video_link, on_progress_callback=progress_function)
+print('Имя файла = ', yt.title)
+for st in yt.streams.filter(progressive=True, file_extension='mp4').all():
+	print(st.itag, st.mime_type, st.resolution)
+video = yt.streams.filter(progressive=True, file_extension='mp4').first()
+print('Размер файла = ', video.filesize//1000000, ' Mb')
+video.download('c:/temp')
+
