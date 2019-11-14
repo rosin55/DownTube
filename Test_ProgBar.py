48427425831d79e706f5caec299f4156540bd6b5
Rosin <asakhno@gmail.com>
2019-11-14 15:01:07 +0300

    Форматирование по PEP8

--- Test_ProgBar.py
+++ Test_ProgBar.py
@@ -3,15 +3,17 @@
 
 import progressbar
 import time
+
 # вывод прогрессбара с кастомным отображением
-pbar = progressbar.ProgressBar(widgets=[progressbar.Bar(fill='-', marker = '|'), progressbar.Percentage(), ' ', progressbar.SimpleProgress()])
+pbar = progressbar.ProgressBar(
+	widgets=[progressbar.Bar(fill='-', marker='|'), progressbar.Percentage(), ' ', progressbar.SimpleProgress()])
 
 for i in pbar(range(50)):
 	time.sleep(0.1)
 
-pbar = progressbar.ProgressBar(widgets=[progressbar.Bar(fill=' ', marker = '.', fill_left=False)]).start()
+pbar = progressbar.ProgressBar(widgets=[progressbar.Bar(fill=' ', marker='.', fill_left=False)]).start()
 for i in range(100):
 	time.sleep(0.1)
-	pbar.update(i+1)
+	pbar.update(i + 1)
 
 pbar.finish()
